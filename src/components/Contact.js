ee3ab185c6b3e7bee737d1c55a1adb43e7855fb8
PriyaRSurendranath <priyami2010@gmail.com>
2021-02-19 16:46:39 -0500

    validating Contact Form using Formik

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,65 +1,94 @@
-import React, { Component } from 'react';
-import { Form, Row, Col, Button, Navbar } from 'react-bootstrap';
-import regeneratorRuntime from 'regenerator-runtime';
-import { Formik} from 'formik';
+import React from 'react';
+import { Component } from 'react';
+import { Row, Col, Navbar, NavbarBrand, Container } from 'reactstrap';
+import { ReactstrapInput } from 'reactstrap-formik';
+import { Formik, Form, Field } from 'formik';
+
 import './Contact.css';
+
 export default class Contact extends Component {
     render() {
         return (
-            <div>
-                <br />
-                <div>
-                    <Navbar variant="light" className="nav-bar">
-                        <Navbar.Brand className="nav-bar-text" href="#home">
-                            <img
-                                alt=""
-                                src="./assets/Contact-icon.png"
-                                width="30"
-                                height="30"
-                                className="d-inline-block align-top"
-                            />{' '}Contact Me
+         <div>
+                <Navbar color="light" light expand="md">
+                    <NavbarBrand href="/">
+                        <img
+                            src="/assets/Contact-icon.png"
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                            alt="React Bootstrap logo"
+                        />{' '}
+                    Contact Me</NavbarBrand>
+                </Navbar>
+          
+                
+             
+                    <Formik
+                        initialValues={{
+                            email: "",
+                            comment: "",
+                        }}
+                        validate={values => {
+                            const errors = {};
 
-                            </Navbar.Brand>
-                    </Navbar>
-                </div>
-                <br />
-                <Formik
-                    initialValues={{
-                        email: '',
-                    }}
-                    onSubmit={async (values) => {
-                        await new Promise((r) => setTimeout(r, 500));
-                        alert(JSON.stringify(values, null, 2));
-                    }}
-                >
-                    <Form>
-                        <Form.Group as={Row} controlId="formHorizontalEmail">
-                            <Form.Label column sm={2}>
-                                Email
-                            </Form.Label>
-                            <Col sm={10}>
-                                <Form.Control  name ="email" type="email" placeholder="Email" />
-                            </Col>
-                        </Form.Group>
+                            if (!values.email) {
+                                errors.email = "Required";
+                            } else if (
+                                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+                            ) {
+                                errors.email = "Invalid email address";
+                            }
+                            if (!values.comment) {
+                                errors.comment = "Required";
+                            }
+                            return errors;
+                        }}
+                        onSubmit={values => {
+                            // Make Email API Calls here
+                            console.log(values);
 
-                        <Form.Group as={Row} controlId="formHorizontalComment">
-                            <Form.Label column sm={2}>
-                                Comment
-                            </Form.Label>
-                            <Col sm={10}>
-                                <Form.Control as="textarea" type="text" placeholder="Comment Here" />
-                            </Col>
-                        </Form.Group>
+                            setTimeout(() => {
+                                setSubmitting(false);
+                                alert(
+                                    `Submitted Successfully ->  ${JSON.stringify(values, null, 2)}`
+                                );
+                            }, 2000);
+                        }}
 
-                        <Form.Group as={Row}>
-                            <Col sm={{ span: 10, offset: 2 }}>
-                                <Button type="submit">Submit</Button>
-                            </Col>
-                        </Form.Group>
-                    </Form>
-                </Formik>
+                        render={({ submitForm, isSubmitting, values }) => (
+                            <Form>
+                                <Container style={{ paddingTop: "5px" }}>
+                                    <Row>
+                                        <Col xs="12">
+                                            <Field
+                                                type="email"
+                                                label="Email"
+                                                name="email"
+                                                id="email"
+                                                component={ReactstrapInput}
+                                            />
+                                        </Col>
+                                        <Col xs="12">
+                                            <Field
+                                                type="textarea"
+                                                label="Comment"
+                                                name="comment"
+                                                id="comment"
+                                                component={ReactstrapInput}
+                                            />
+                                        </Col>
+
+                                        <Col xs="12">
+                                            <button type="submit">Submit</button>
+                                        </Col>
+                                    </Row>
+                                    <pre>{JSON.stringify(values, null, 2)}</pre>
+                                </Container>
+                            </Form>
+                        )}
+                    />
             </div>
         )
     }
 }
-

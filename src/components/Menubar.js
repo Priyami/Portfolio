b31e0a89026baa112986f995f71e78e248b9cfbf
Priya Ramachanriya Surendranath <priyami2010@gmail.com>
2021-05-14 09:38:33 -0400

    deleteall button included in report page

--- src/components/Menubar.js
+++ src/components/Menubar.js
@@ -9,10 +9,10 @@ export default class Menubar extends Component {
         return (
             <div>
                 <Navbar bg="dark" variant="dark">
-                    <Navbar.Brand href="#home">Portfolio</Navbar.Brand>
+                    <Navbar.Brand href="/">Portfolio</Navbar.Brand>
                     <Nav className="mr-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#project">Project</Nav.Link>
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/Project">Project</Nav.Link>
                         <Nav.Link href="https://docs.google.com/document/d/1A6W_kIqgbzozeMr541f40zJhCjQIc-zl5gMdB7f5Acw/edit?usp=sharing">Resume</Nav.Link>
                     </Nav>
                     <Form inline>
--- src/components/Report.js
+++ src/components/Report.js
@@ -2,7 +2,10 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import VisitorTable from '../service/VisitorTable';
 import ReactTable from '../service/ReactTable';
-import {Container, Row, Col} from 'react-bootstrap';
+import  Card  from 'react-bootstrap/Card';
+import { Row, Col, Navbar, NavbarBrand, Container } from 'reactstrap';
+
+import Menubar from '../components/Menubar';
 
 
 const useVisit = () => {
@@ -21,25 +24,62 @@ const useVisit = () => {
     return visitor
 }
 
+const handleDeleteAll = () => {
+   
+        axios.delete('http://localhost:4000/users')
+        .then((result) => {
+            console.log("Document successfully deleted!"); 
+        })
+            
+}
+
 const Report = () => {
     const visitor = useVisit();
     return (
         <div>
-             <React.Fragment>
-            <Container>
-           
-                <Col>
-                    <h1>Registered Visitor</h1>
+            <React.Fragment>
+                <Container>
+                    <Row>
+                        <Col>
+                            <Menubar></Menubar>
+                        </Col>
+                    </Row>
+                    
+                    <br />
+                    <Card body>
+                    <Row>
                    
-                    
-                    <ReactTable visitor = {visitor}></ReactTable>
-                </Col>
-           
-            <br/>
-                    
-            </Container>
+                        <Col>
+                            <Navbar color="light" light expand="md">
+                                <NavbarBrand href="/">
+                                    <img
+                                        src="images/Contact-icon.png"
+                                        width="30"
+                                        height="30"
+                                        className="d-inline-block align-top"
+                                        alt="React Bootstrap logo"
+                                    />{' '}
+                                    Visitors
+                                </NavbarBrand>
+                            </Navbar>
+
+                            <br />
+                            <ReactTable visitor={visitor}></ReactTable>
+                        </Col>
+
+                        <Col xs="12">
+    
+                                    <button type="submit" id="delete" onClick = {handleDeleteAll}>Delete All</button>
+                                    
+                        </Col>
+                        
+                    </Row>
+                    </Card>
+                    <br />
+
+                </Container>
             </React.Fragment>
-         
+
         </div>
     )
 }

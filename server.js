ad3d9090e995d65273d55fa22f3238efae34ea14
PriyaRSurendranath <priyami2010@gmail.com>
2021-12-06 17:17:56 -0500

    path setup

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, '..', 'dist')));
 
 app.get('/', function (req, res) {
- res.sendFile(path.join(__dirname,'..', 'src', 'index.html'));
+ res.sendFile(path.join(__dirname,'..', 'dist', 'build.js'));
 });
 
 app.listen(PORT);

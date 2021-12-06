4e3aee5aa77f51ecc6ea766ee436434c31105db9
PriyaRSurendranath <priyami2010@gmail.com>
2021-12-06 17:40:07 -0500

    make it better

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, '..', 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/', function (req, res) {
- res.sendFile(path.join(__dirname,'..', 'dist', 'build.js'));
-});
+
 
 app.listen(PORT);
